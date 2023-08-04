396a36c2903b701d709053bad8f6e51f823fca2c
Frode V. Fjeld <frodevf@gmail.com>
2023-08-05 00:01:03 +0200

    Add async_unload_entry

--- custom_components/nilan_cts600/__init__.py
+++ custom_components/nilan_cts600/__init__.py
@@ -7,3 +7,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up the integration from a (UI) config entry."""
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    return True
