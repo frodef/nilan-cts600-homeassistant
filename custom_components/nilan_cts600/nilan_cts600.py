e381018a8ca2ebde73f8bb26e731ba08f40a7724
Niels-Ole Gram <niels-ole.gram@zentriq.com>
2023-09-08 17:27:07 +0200

    Added support for scanning the program which may or may not be set.

--- custom_components/nilan_cts600/nilan_cts600.py
+++ custom_components/nilan_cts600/nilan_cts600.py
@@ -453,10 +453,10 @@ class CTS600:
                     display = run_action(e['display']) if 'display' in e else self.display()
                     # print (f"scan: {e}, disp {display}")
                     if 'regexp' in e:
-                        if not (match := re.match (e['regexp'], display)):
+                        if not (match := re.match (e['regexp'], display)) and ('default' not in e):
                             break # Stop sequence at first mismatch
                         else:
-                            record_matching_entry (match.groupdict(), e)
+                            record_matching_entry (match.groupdict() if match else { 'value': e['default'] }, e)
                             if 'gonext' in e:
                                 run_action(e['gonext'])
                 else:
@@ -504,6 +504,7 @@ class CTS600:
             f (regexp="(?P<value>.*)", var='display', parse=lambda d: d.replace ('/', '\n')),
             f (regexp=".* (?P<value>\d+)°C", var='thermostat', parse=int),
             f (regexp="^(?P<value>\w+)", var='mode'),
+            f (regexp="^\w+\s+(?P<value>\w)", var='program', default=None),
             f (regexp=".*>(?P<value>\d+)<", var='flow', parse=int)
         ]
         if updateShowData:
